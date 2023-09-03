4391ad58d420a85e3e5e2cded99f4eb8efde843c
Brian Mai <0hrs3g@gmail.com>
2023-09-03 16:47:46 -0700

    change body display

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col items-center justify-center min-h-screen bg-pink-200`}>{children}</body>
+      <body className={`${inter.className} flex flex-col items-center justify-center min-h-screen`}>{children}</body>
     </html>
   )
 }
