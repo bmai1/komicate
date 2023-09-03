22faea89caacb970356d5a199d261e300948ab57
Brian Mai <0hrs3g@gmail.com>
2023-09-03 16:48:04 -0700

    add info card

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import BlueButton from './components/BlueButton';
+import Button from './components/Button';
+import InfoCard from './components/InfoCard';
 
+
+// react components can only return single parent element
 export default function Page() {
     return (
-        <div>
-            <h1>Hello, Himeko.</h1>
-            <BlueButton />
+        <div className="flex flex-col justify-center items-center">
+            <p className="text-xl text-rose-400">This page is dedicated to Himeko Inaba.</p>
+            <Button />
+            <InfoCard />
         </div>
     );
 }
