6820fa0092d91b8f4cfc7d9404343fe06362109c
Brian Mai <0hrs3g@gmail.com>
2023-09-03 16:48:11 -0700

    rm

--- app/components/BlueButton.tsx
+++
@@ -1,9 +0,0 @@
-function BlueButton() {
-    return (
-      <button className="bg-blue-500 text-white rounded-full py-2 px-4 hover:bg-blue-600 focus:outline-none">
-        Hello World
-      </button>
-    );
-}
-
-export default BlueButton;
