265f8eddee36818a7d00b00eaf0cd860f9af053e
Brian Mai <0hrs3g@gmail.com>
2023-09-07 18:22:36 -0700

    dynamic css class function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
+// useful for dynamic css classes (based on states)
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
