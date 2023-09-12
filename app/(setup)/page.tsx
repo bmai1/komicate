f8da11ca2a0ba349c2681f37737d075622f8a9d1
Brian Mai <0hrs3g@gmail.com>
2023-09-11 17:09:41 -0700

    modify import

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
-import { InitialModal } from "@/components/modals/initial-model";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
