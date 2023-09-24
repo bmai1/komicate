bf5c68e8e0d4b66df21386b2a8620a82eaf591ae
Brian Mai <0hrs3g@gmail.com>
2023-09-24 14:54:30 -0700

    add size to stop warning

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -46,6 +46,7 @@ export const NavigationItem = ({
                 )}>
                     <Image 
                         fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                         src={imageUrl}
                         alt="Channel"
                     />
