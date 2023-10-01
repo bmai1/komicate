8cf5a6002f20aeeb2c8bf0a5dad36f21029b2589
Brian Mai <0hrs3g@gmail.com>
2023-10-01 16:43:20 -0700

    explore button

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Plus } from "lucide-react";
+import { Plus, Compass } from "lucide-react";
 
 import { ActionTooltip } from "@/components/action-tooltip";
 import { useModal } from "@/hooks/use-modal-store";
@@ -26,6 +26,21 @@ export const NavigationAction = () => {
                     />
                 </div>
             </button>
+           </ActionTooltip>
+           <ActionTooltip
+            side="right"
+            align="center"
+            label="Explore Servers"
+           >
+            <button 
+                  className="pt-2 group flex items-center"
+              >
+                  <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
+                      <Compass 
+                      className="group-hover:text-white transition text-emerald-500 size={25}"
+                      />
+                  </div>
+              </button>
            </ActionTooltip>
         </div>
     )
