f2c3e061ad414e6a0387e899404bb46dc3d512fc
Brian Mai <0hrs3g@gmail.com>
2023-11-06 11:15:33 -0800

    onclicks for leave/delete modals

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -95,6 +95,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("deleteServer", { server })}
                         className="text-rose-400 px-3 py-2 text-sm cursor-pointer"
                     >
                         Delete Server
@@ -103,6 +104,7 @@ export const ServerHeader = ({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("leaveServer", { server })}
                         className="text-rose-400 px-3 py-2 text-sm cursor-pointer"
                     >
                         Leave Server
