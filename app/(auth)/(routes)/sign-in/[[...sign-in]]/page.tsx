566ccf35894c72c2998c502ff26d38edb502dc5a
Brian Mai <0hrs3g@gmail.com>
2023-11-27 21:29:55 -0800

    route groups, optional catch-all segment

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,14 @@
+// app/(auth)/(routes)/sign-in/[[..sign-in]]/page.ts
+
+// (auth) - parentheses creates a route group for organization, not included in route url path
+//          https://nextjs.org/docs/app/building-your-application/routing/colocation
+
+// [[..sign-in]] - optional catch-all segment with ellipsis and double square brackets
+//                 https://nextjs.org/docs/pages/building-your-application/routing/dynamic-routes
+
 import { SignIn } from "@clerk/nextjs";
  
+// default exports used for single value per file as opposed to name exports
 export default function Page() {
   return <SignIn />;
 }
