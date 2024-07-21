f10e74c9b53678f389bd18c3f04377839ea510c7
Brian Mai <0hrs3g@gmail.com>
2024-07-21 00:32:06 -0700

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,11 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: [
-            "uploadthing.com",
-            "utfs.io"
-        ]
-    }
-}
+// const nextConfig = {
+//     images: {
+//         domains: [
+//             "uploadthing.com",
+//             "utfs.io"
+//         ]
+//     }
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// next 14
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        pathname: '**',
+      },
+    ],
+  },
+}
