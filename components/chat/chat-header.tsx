6fd988c5e4ae145d6b29e8d0e11f7a272b61dd25
Brian Mai <0hrs3g@gmail.com>
2025-11-05 09:23:01 -0800

    feat(chat-header): correct icon map for channels

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,9 @@
-import { db } from "@/lib/db";
 import { UserAvatar } from "@/components/user-avatar";
+import { Hash, MessageSquare, Mic, Video } from "lucide-react";
+import { db } from "@/lib/db"
+import { redirect } from "next/navigation";
+import { ChannelType } from "@prisma/client";
+import { SocketIndicator } from "@/components/providers/socket-indicator";
 
 interface ChatHeaderProps {
     serverId: string;
@@ -7,14 +11,44 @@ interface ChatHeaderProps {
     type: "channel" | "conversation";
     imageUrl?: string;
 }
+
+
 export const ChatHeader = async ({
     serverId,
     name,
     type,
     imageUrl
 }: ChatHeaderProps) => {
+    // Can't pass in a channel for channel.type icon mapping since chat headers also used for dms with server members, so just get server
+    const iconMap = {
+        [ChannelType.TEXT]: <Hash className="h-5 mr-2"/>,
+        [ChannelType.AUDIO]: <Mic className="h-5 mr-2"/>,
+        [ChannelType.VIDEO]: <Video className="h-5 mr-2"/>
+    };
+    let channelType = null;
+
+    if (type === "channel") {
+        const server = await db.server.findUnique({
+            where: {
+                id: serverId,
+            },
+            include: {
+                channels: {
+                    where: {
+                        name: name
+                    }
+                },
+            }
+        });
+        if (!server) return <div>Error finding server</div>;
+    
+        channelType = server.channels[0].type;
+        if (!channelType) return <div>Error finding channel</div>;
+    }
+    
     return (
         <div className="px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+            {type === "channel" && channelType && iconMap[channelType] }
             {type === "conversation" && (
                 <UserAvatar 
                     src={imageUrl}
@@ -22,6 +56,9 @@ export const ChatHeader = async ({
                 />
             )}
             <p className="dark:text-white">{name}</p>
+            <div className="ml-auto">
+                <SocketIndicator />
+            </div>
         </div>
     )
 }
