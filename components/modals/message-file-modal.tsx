ee272c5f3ab7309b46a0f3ccf836b304bf1c5207
Brian Mai <0hrs3g@gmail.com>
2025-11-05 21:28:07 -0800

    feat(upload): upload images and pdfs to uploadthing

--- components/modals/message-file-modal.tsx
+++ components/modals/message-file-modal.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import axios from "axios";
+import qs from "query-string";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form";
-import { useEffect, useState } from "react";
 
 import { 
     Dialog,
@@ -19,40 +19,36 @@ import {
     FormControl,
     FormField, 
     FormItem,
-    FormLabel,
-    FormMessage
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
 import { useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-modal-store";
 
 const formSchema = z.object({
-    name: z.string().min(1, {
-        message: "Server name is required."
-    }),
-    imageUrl: z.string().min(1, {
-        message: "Server image is required."
+    fileUrl: z.string().min(1, {
+        message: "Attachment is required."
     })
 });
 
-export const InitialModal = () => {
-    // modals often cause React hydration errors (improper wrapping tags)
-    const [isMounted, setIsMounted] = useState(false);
-    
+export const MessageFileModal = () => {
+    const { isOpen, onClose, type, data } = useModal();
     const router = useRouter();
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, [])
+    const isModalOpen = isOpen && type === "messageFile";
+    const { apiUrl, query } = data;
 
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            name: "",
-            imageUrl: "",
+            fileUrl: "",
         }
     });
+
+    const handleClose = () => {
+        form.reset();
+        onClose();
+    }
 
     const isLoading = form.formState.isSubmitting;
 
@@ -60,26 +56,34 @@ export const InitialModal = () => {
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         console.log(values);
         try {
-            await axios.post("/api/servers", values);
+            const url = qs.stringifyUrl({
+                url: apiUrl || "",
+                query,
+            });
+
+            await axios.post(url, {
+                ...values,
+                content: values.fileUrl,
+            });
             
             form.reset();
             router.refresh();
-            window.location.reload();
+            handleClose();
+
         } catch (error) {
             console.log(error)
         }
     }
 
-    if (!isMounted) return null;
     return (
-        <Dialog open>
+        <Dialog open={isModalOpen} onOpenChange={handleClose}>
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
-                        Customize your server
+                        Add an attachment
                     </DialogTitle>
                     <DialogDescription className="text-center text-zinc-500">
-                        Upload a server icon and enter a name for your server.
+                        Send a file as a message
                     </DialogDescription>
                 </DialogHeader>
                 <Form {...form}>
@@ -88,12 +92,12 @@ export const InitialModal = () => {
                             <div className="flex items-center justify-center text-center">
                                 <FormField 
                                     control={form.control}
-                                    name="imageUrl"
+                                    name="fileUrl"
                                     render={({field}) => (
                                         <FormItem>
                                             <FormControl>
                                                 <FileUpload 
-                                                    endpoint="serverImage"
+                                                    endpoint="messageFile"
                                                     value={field.value}
                                                     onChange={field.onChange}
                                                 />
@@ -103,31 +107,11 @@ export const InitialModal = () => {
                                 />
                             </div>
 
-                            <FormField
-                                control={form.control}
-                                name="name"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel className="uppercase text-sx font-bold text-zinc-500 dark:text-secondary/70">
-                                            Server name
-                                        </FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                disabled={isLoading}
-                                                className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                                                placeholder="Enter server name"
-                                                {...field}
-                                                autoComplete="off"
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                             />
+                        
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4">
                             <Button variant={"primary"} disabled={isLoading}>
-                                Create
+                                Send
                             </Button>
                         </DialogFooter>
                     </form>
