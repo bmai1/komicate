c5c759a39422bd956f01a31469f3ceeba43043eb
Brian Mai <0hrs3g@gmail.com>
2025-11-07 07:28:29 -0800

    style: chat welcome

--- components/chat/chat-welcome.tsx
+++ components/chat/chat-welcome.tsx
@@ -8,16 +8,14 @@ export const ChatWelcome = ({
     type,
 }: ChatWelcomeProps) => {
     return (
-        <div className="space-y-2 px-4">
-       
-
-        <p className="text-xl md:text-3xl font-bold">
-            {type === "channel" ? "Welcome to #" : ""}{name}.
-        </p>
-        <p className="text-zinc-600 dark:text-zinc-400 text-sm">
-            This could be the start of something. 
-        </p>
-            
+        <div className="space-y-2 px-4 mb-4">
+            <p className="text-xl md:text-3xl font-bold">
+                {type === "channel" ? "Welcome to #" : ""}{name}.
+            </p>
+            <p className="text-zinc-600 dark:text-zinc-400 text-sm">
+                This could be the start of something. 
+            </p> 
+            <hr className="border-0 h-[0.5px] bg-zinc-600"></hr>
         </div>
     )
 }
