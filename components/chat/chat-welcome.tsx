6db8ec839f84f8f6827ffd37a5f5c45372bc73a8
Brian Mai <0hrs3g@gmail.com>
2025-11-08 02:09:42 -0800

    fix: remove period

--- components/chat/chat-welcome.tsx
+++ components/chat/chat-welcome.tsx
@@ -10,7 +10,7 @@ export const ChatWelcome = ({
     return (
         <div className="space-y-2 px-4 mb-4">
             <p className="text-xl md:text-3xl font-bold">
-                {type === "channel" ? "Welcome to #" : ""}{name}.
+                {type === "channel" ? "Welcome to #" : ""}{name}
             </p>
             <p className="text-zinc-600 dark:text-zinc-400 text-sm">
                 This could be the start of something. 
